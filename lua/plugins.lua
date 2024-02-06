0cf7ac93f434eb67ad8f37ffbf01bf02cf94bc1f
clay <dcheng69@uwo.ca>
2024-02-06 13:28:24 -0500

    Modify the packer.nvim plugin
    [Ticket: packer.nvim]
    
    [body]
    1. Modify the packer.nvim according to the LunarNvim
    2. Reconfigure the config code
    
    [test]
    none

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,49 @@
-local packer = require("packer")
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+ PACKER_BOOTSTRAP = fn.system({
+  "git",
+  "clone",
+  "--depth",
+  "1",
+  "https://github.com/wbthomason/packer.nvim",
+  install_path,
+ })
+ print("Installing packer close and reopen Neovim...")
+ vim.cmd([[packadd packer.nvim]])
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+ return
+end
+
+-- Have packer use a popup window
+packer.init({
+ max_jobs = 16,
+ compile_on_sync = true,
+ display = {
+  open_fn = function()
+   return require("packer.util").float({ border = "rounded" })
+  end,
+ },
+ -- self define source
+ git = {
+  -- default_url_format = "https://github.com/%s",
+ },
+})
+
 packer.startup({
   function(use)
     -- Packer can manage itself
@@ -88,31 +133,12 @@ packer.startup({
     use {"L3MON4D3/LuaSnip"} -- snippet engine
     use {"rafamadriz/friendly-snippets"} -- a bunch of snippets to use
     -------------------------------------------------------
-end,
-  config = {
-    max_jobs = 16,
-    -- self define source
-    git = {
-      -- default_url_format = "https://hub.fastgit.xyz/%s",
-      -- default_url_format = "https://mirror.ghproxy.com/https://github.com/%s",
-      -- default_url_format = "https://gitcode.net/mirrors/%s",
-      -- default_url_format = "https://gitclone.com/github.com/%s",
-    },
-    display = {
-        open_fn = function()
-            return require("packer.util").float({ border = "single" })
-        end,
-    },
-  },
-})
 
--- function for auto install plugin when we save this file
-pcall(
-  vim.cmd,
-  [[
-    augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-    augroup end
-  ]]
-)
+    -------------------------------------------------------
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if PACKER_BOOTSTRAP then
+     require("packer").sync()
+    end
+end,
+})
