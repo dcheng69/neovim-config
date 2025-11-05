6f3876d7fbb644b8e5de7f2c8cd74514111707d8
Clay Cheng <ccheng@tigercat.com>
2025-11-05 09:34:55 -0500

    Add vscode-nvim support

--- init.lua
+++ init.lua
@@ -1,25 +1,31 @@
+-- init.lua
 -- basic configuration
 require('basic')
--- keybindings
-require('keybindings')
--- plugins
-require('plugins')
--- color scheme
-require('colorscheme')
--- plugin configuration
-require('plugin-config.nvim-tree')
--- buffer-line
-require("plugin-config.bufferline")
--- lualine
-require("plugin-config.lualine")
--- telescope
-require("plugin-config.telescope")
--- dashboard
-require("plugin-config.dashboard")
--- project
-require("plugin-config.project")
---cmp
-require("plugin-config.cmp")
--- lsp
-require("lsp-config")
+if vim.g.vscode then
+    -- VSCode extension
+else
+    -- ordinary Neovim configurations
+    -- keybindings
+    require('keybindings')
+    -- plugins
+    require('plugins')
+    -- color scheme
+    require('colorscheme')
+    -- plugin configuration
+    require('plugin-config.nvim-tree')
+    -- buffer-line
+    require("plugin-config.bufferline")
+    -- lualine
+    require("plugin-config.lualine")
+    -- telescope
+    require("plugin-config.telescope")
+    -- dashboard
+    require("plugin-config.dashboard")
+    -- project
+    require("plugin-config.project")
+    --cmp
+    require("plugin-config.cmp")
+    -- lsp
+    require("lsp-config")
+end
 
