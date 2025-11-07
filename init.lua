cba40729c3d7db3eab3d335f8a7cde8775b07706
Clay Cheng <ccheng@tigercat.com>
2025-11-07 09:10:06 -0500

    Add more keybindings

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@
 require('basic')
 if vim.g.vscode then
     -- VSCode extension
+    -- https://github.com/vscode-neovim/vscode-neovim/issues/298
+    vim.opt.clipboard:append("unnamedplus")
 else
     -- ordinary Neovim configurations
     -- keybindings
